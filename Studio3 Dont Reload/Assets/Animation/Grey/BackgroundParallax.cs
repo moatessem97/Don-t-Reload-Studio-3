db2c35ac7d63ed00ca7856cec2bf80c112a10357
Moatassem Waly <m.waly@saedubai.com>
2017-07-24 18:54:38 +0400

    Revert "Revert "Animation Complete""
    This reverts commit 2b7107d38d89fbb8b92df416faa62ef22eb5a248.

---
+++ Studio3 Dont Reload/Assets/Animation/Grey/BackgroundParallax.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BackgroundParallax : MonoBehaviour {
+	public float Speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 offset = new Vector2 (0,Time.time * Speed);
+		GetComponent<Renderer>().material.mainTextureOffset = offset;
+
+	}
+}
