8ca3a6acdfe0d4f2960b2309361a1d89b6c7160d
Moatassem Waly <m.waly@saedubai.com>
2017-07-25 10:32:23 +0400

    Added Harambe

---
+++ Studio3 Dont Reload/Assets/MoveSprite.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSprite : MonoBehaviour {
+	public Vector3 target;
+	public float speed;
+	private Vector3 position;
+	// Use this for initialization
+	void Start () {
+		target = new Vector3 (90, 4, 0);
+		position = gameObject.transform.position;
+		 speed = 0.6f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target, step);
+	}
+}
