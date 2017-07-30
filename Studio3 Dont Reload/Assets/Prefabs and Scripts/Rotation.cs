2584648c5aaab3e410dc1d8b819545d6a7120464
Moatassem Waly <m.waly@saedubai.com>
2017-07-30 20:30:42 +0400

    Scenery boiz

---
+++ Studio3 Dont Reload/Assets/Prefabs and Scripts/Rotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+	public float speed = 2.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+
+
+	
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate(Vector3.forward, 45 * Time.deltaTime * speed);
+	}
+}
