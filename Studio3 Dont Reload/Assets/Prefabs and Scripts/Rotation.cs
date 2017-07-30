5f28ba7d35602ce59b183504cc134a35b8b9bd46
Moatassem Waly <m.waly@saedubai.com>
2017-07-31 01:06:15 +0400

    New Map

--- Studio3 Dont Reload/Assets/Prefabs and Scripts/Rotation.cs
+++ Studio3 Dont Reload/Assets/Prefabs and Scripts/Rotation.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour {
 	public float speed = 2.5f;
+	public Transform child;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +19,7 @@ public class Rotation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		child.transform.rotation = Quaternion.Euler (0.0f, 0.0f, gameObject.transform.rotation.z * -1.0f);
 		transform.Rotate(Vector3.forward, 45 * Time.deltaTime * speed);
 	}
 }
