d7c64dd4b1112ce5f165b68832e1063d3a64cdf5
Sasan <s.faizollahzadeh@saedubai.com>
2017-07-31 10:08:40 +0400

    killzone added but not functioning properly

---
+++ Studio3 Dont Reload/Assets/Prefabs and Scripts/Killzone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+
+public class Killzone : PunBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        GameObject player;
+        if(col.tag == "Player")
+        {
+            Debug.Log("Entered Zone " + col.name);
+            player = col.transform.root.gameObject;
+            
+        }
+    }
+}
